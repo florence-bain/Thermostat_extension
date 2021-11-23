d7b4432f7870ecd404b04a1ba2d40228a6eac575
Florence Bain <florence_bain@outlook.com>
2021-11-23 11:50:34 +0000

    can decrease the temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -16,4 +16,9 @@ describe('Thermostat', () => {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
+
+  it('decreases in temperature', () => {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
 });
