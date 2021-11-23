bfa9d7b31fa47cc8c313df66f01c890ba9c90044
Florence Bain <florence_bain@outlook.com>
2021-11-23 12:04:42 +0000

    set power save mode on by default

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,6 +4,7 @@ class Thermostat {
   constructor() {
     this.temperature = 20;
     this.MINIMUM_TEMPERATURE = 10;
+    this.powerSavingMode = true;
   }
   getCurrentTemperature() {
     return this.temperature;
@@ -19,5 +20,8 @@ class Thermostat {
   }
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
   }
 };
