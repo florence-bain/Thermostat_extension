f3056244bfe7065525f08704aa5225768a78d900
Florence Bain <florence_bain@outlook.com>
2021-11-25 19:33:11 +0000

    updating cities

--- src/Cities.js
+++ src/Cities.js
@@ -7,59 +7,59 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01
     document.querySelector('#current-temperature-london').innerText = data.main.temp;
   });
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Cardiff,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Sydney,&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    document.querySelector('#current-temperature-cardiff').innerText = data.main.temp;
+    document.querySelector('#current-temperature-sydney').innerText = data.main.temp;
   });
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Edinburgh,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Milan,it&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    document.querySelector('#current-temperature-edinburgh').innerText = data.main.temp;
+    document.querySelector('#current-temperature-milan').innerText = data.main.temp;
   });
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Beijing,cn&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    document.querySelector('#current-temperature-dublin').innerText = data.main.temp;
+    document.querySelector('#current-temperature-beijing').innerText = data.main.temp;
   });
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Bristol,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=New York,us&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    document.querySelector('#current-temperature-bristol').innerText = data.main.temp;
+    document.querySelector('#current-temperature-newyork').innerText = data.main.temp;
   });
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Glasgow,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Rio de janeiro,br&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    document.querySelector('#current-temperature-glasgow').innerText = data.main.temp;
+    document.querySelector('#current-temperature-riodejaneiro').innerText = data.main.temp;
   });
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Liverpool,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Singapore,sg&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    document.querySelector('#current-temperature-liverpool').innerText = data.main.temp;
+    document.querySelector('#current-temperature-singapore').innerText = data.main.temp;
   });
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Newcastle upon Tyne,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Osaka,jp&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
   .then((response) => {
     return response.json()
   })
   .then((data) => {
-    document.querySelector('#current-temperature-newcastle').innerText = data.main.temp;
+    document.querySelector('#current-temperature-osaka').innerText = data.main.temp;
   });
 
