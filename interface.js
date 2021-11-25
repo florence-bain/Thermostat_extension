f5ace50753f82761184620e35686eca6d22cfaf3
Florence Bain <florence_bain@outlook.com>
2021-11-25 15:23:13 +0000

    fixing errors with the DOM

--- interface.js
+++ interface.js
@@ -1,58 +1,60 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.querySelector('#select-city').addEventListener('submit', (event) => {
+  event.preventDefault();
   const displayWeather = (city) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+  
     fetch(url)
       .then((response) => {
         return response.json()
       })
       .then((data) => {
         document.querySelector('#current-temperature').innerText = data.main.temp;
-      });
-  };
+      })
+    };
 
   displayWeather('London');
 
   document.querySelector('#select-city').addEventListener('submit', (event) => {
-    event.preventDefault();
-    const city = document.querySelector('#current-city').value;
+  event.preventDefault();
+  const city = document.querySelector('#current-city').value;
 
-    displayWeather(city);
-  })
+  displayWeather(city);
+  });
+
 
   const updateTemperature = () => {
     document.querySelector('#temperature').innerText = thermostat.temperature;
     document.querySelector('#temperature').className = thermostat.energyUsage();
-  }
+  };
 
   const thermostat = new Thermostat();
   updateTemperature();
 
-  document.querySelector('#temperature-up').addEventListener('click', () => { // event listener
-    thermostat.up(); // update model
-    updateTemperature();// update view
-  })
+  document.querySelector('#temperature-up').addEventListener('click', () => { 
+    thermostat.up(); 
+    updateTemperature();
+  });
 
   document.querySelector('#temperature-down').addEventListener('click', () => {
     thermostat.down();
     updateTemperature();
-  })
+  });
 
   document.querySelector('#temperature-reset').addEventListener('click', () => {
     thermostat.resetTemperature();
     updateTemperature();
-  })
+  });
 
   document.querySelector('#PSM-on').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOn();
     document.querySelector('#power-saving-status').innerText = 'On';
     updateTemperature();
-  })
+  });
 
   document.querySelector('#PSM-off').addEventListener('click', () => {
     thermostat.switchPowerSavingModeOff();
     document.querySelector('#power-saving-status').innerText = 'Off';
     updateTemperature();
   })
-
-})
+});
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -11,42 +11,52 @@ class Thermostat {
     this.MEDIUM_ENERGY_USAGE = 18;
     this.HIGH_ENERGY_USAGE = 25;
   }
+
   getCurrentTemperature() {
     return this.temperature;
   }
+
   up() {
     if (this.isMaximumTemperature()) {
       return;
     }
     this.temperature += 1
   }
+
   down() {
     if (this.isMinimumTemperature()) {
       return;
     }
     this.temperature -= 1
   }
+
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
   }
+
   isPowerSavingModeOn() {
     return this.powerSavingMode === true;
   }
+
   switchPowerSavingModeOff() {
     this.powerSavingMode = false;
   }
+
   switchPowerSavingModeOn() {
     this.powerSavingMode = true;
   }
+
   isMaximumTemperature() {
     if (this.isPowerSavingModeOn() === false) {
       return this.temperature === this.MAX_LIMIT_PSM_OFF;
     }
     return this.temperature === this.MAX_LIMIT_PSM_ON;
   }
+
   resetTemperature() {
     this.temperature = this.DEFAULT_TEMPERATURE;
   }
+  
   energyUsage() {
     if (this.temperature < this.MEDIUM_ENERGY_USAGE) {
       return 'low-usage';
